7bf1cd1b6181465c7e73afc3f79f817b6e1b333f
Sayem92 <sayemmiha123@gmail.com>
2022-09-24 23:09:57 +0600

    add cart component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+const {img, name, price, seller, ratings} = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+           <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: {price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+           </div>
+
+           <button className='btn-cart'>
+            <p>Add to Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
