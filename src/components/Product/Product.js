3045fb16c52a795a1af16854b1ce39f3643e2274
Sayem92 <sayemmiha123@gmail.com>
2022-09-25 12:10:46 +0600

    add font awesome

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-const {img, name, price, seller, ratings} = props.product
+ // clcik ar all function props a aice so aikane click or
+const {img, name, price, seller, ratings} = props.product;
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
            <div className='product-info'>
                 <p className='product-name'>{name}</p>
-                <p>Price: {price}</p>
+                <p>Price: ${price}</p>
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
            </div>
 
-           <button className='btn-cart'>
-            <p>Add to Cart</p>
+           <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'>
+            <p className='btn-text'>Add to Cart</p>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,12 +6,21 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    } , [])
+    } , []);
+
+    const handleAddToCart = (product) =>{
+      console.log(product)
+    //   cart.push(product)
+
+    const newCart = [...cart, product];
+        setCart(newCart)
+    }
 
     return (
         <div className='shop-container'>
@@ -20,12 +29,14 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                         ></Product>)
                 }
 
             </div>
             <div className="cart-container">
                 <h4>Orders summary</h4>
+                <p>Selected items: {cart.length}</p>
             </div>
         </div>
     );
